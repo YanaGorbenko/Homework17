54145c7d7907f987f312bcff0dce90a68ab370ba
Yana Horbenko <yanagorbenko33@gmail.com>
2025-11-26 14:52:06 +0200

    add adaptation

---
+++ modal.js
@@ -0,0 +1,13 @@
+(() => {
+  const openModalBtn = document.querySelector('[data-modal-open]');
+  const closeModalBtn = document.querySelector('[data-modal-close]');
+  const modal = document.querySelector('[data-modal]');
+
+  openModalBtn.addEventListener('click', toggleModal);
+  closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    modal.classList.toggle('hidden');
+  }
+})();
